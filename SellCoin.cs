b8e9bf95c6807e71a10171c63148ad3199524948
AlphaZ55 <79116881+AlphaZ55@users.noreply.github.com>
2021-02-15 22:01:08 +0000

    r

---
+++ SellCoin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellCoin : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public void ClickTheButton () {
+        GlobalCoins.CoinCount -= 1;
+        GlobalCash.CashCount += 1;
+    }
+}
