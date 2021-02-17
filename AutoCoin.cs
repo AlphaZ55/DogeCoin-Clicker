42eada6af8909ba44d74c104e5b157310d563160
AlphaZ55 <79116881+AlphaZ55@users.noreply.github.com>
2021-02-17 22:14:41 +0000

    New Script for a AutoCoin

---
+++ AutoCoin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCoin : MonoBehaviour
+{
+    
+    public bool CreatingCoin = false;
+    public static int CoinIncrease = 1;
+    public int InternalIncrease;
+    void Update()
+    {
+        InternalIncrease = CoinIncrease;
+        if (CreatingCoin == false )
+        {
+            CreatingCoin = true;
+            StartCoroutine(CreateTheCoin());
+        }
+    }
+    IEnumerator CreateTheCoin ()
+    {
+        GlobalCoins.CoinCount += InternalIncrease;
+        yield return new WaitForSeconds(1);
+        CreatingCoin = false;
+    }
+}
