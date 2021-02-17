014456e94aa099ced3765ad8280871b3b2dc93fa
AlphaZ55 <79116881+AlphaZ55@users.noreply.github.com>
2021-02-17 22:15:21 +0000

    Upgrade System Core

---
+++ PurchaseLog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseLog : MonoBehaviour
+{
+
+    public GameObject AutoCoin;
+
+    public void StartAutoCoin () {
+        AutoCoin.SetActive(true);
+    }
+
+}
