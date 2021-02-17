cdb77954e724cc769df99c9722a4ebd9d77567b7
AlphaZ55 <79116881+AlphaZ55@users.noreply.github.com>
2021-02-17 22:12:11 +0000

    Update 1 - Animation and Fixing Of Infinite Sell

--- SellCoin.cs
+++ SellCoin.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SellCoin : MonoBehaviour
-{
+public class SellCoin : MonoBehaviour {
+
     public GameObject textBox;
+    public GameObject statusBox;
 
     public void ClickTheButton () {
+
+        if (GlobalCoins.CoinCount == 0) 
+        {
+        statusBox.GetComponent<Text>().text = "Not enough Coins to sell!";
+        statusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else 
+        {
         GlobalCoins.CoinCount -= 1;
         GlobalCash.CashCount += 1;
+        }
     }
 }
