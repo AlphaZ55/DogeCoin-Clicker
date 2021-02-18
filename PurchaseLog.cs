4a383f180491cd4fea1bbb9737481a2b9e8f0f1b
AlphaZ55 <79116881+AlphaZ55@users.noreply.github.com>
2021-02-18 22:46:31 +0000

    Update 3 - Audio Support

--- PurchaseLog.cs
+++ PurchaseLog.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PurchaseLog : MonoBehaviour
+public AudioSource playSound;
 {
 
     public GameObject AutoCoin;
 
     public void StartAutoCoin () {
+        playSound.Play();
         AutoCoin.SetActive(true);
         GlobalCash.CashCount -= GlobalMiner.minerValue;
         GlobalMiner.minerValue *=2;
         GlobalMiner.turnOffButton = true;
+        GlobalMiner.minerPerSec += 1;
+        GlobalMiner.numberOfMiners += 1;
     }
 
 }
