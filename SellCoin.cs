44539c2eda0f55dae67aeda3d5b6cea74004c25a
AlphaZ55 <79116881+AlphaZ55@users.noreply.github.com>
2021-02-18 22:47:50 +0000

    Update 3 - Audio

--- SellCoin.cs
+++ SellCoin.cs
@@ -7,9 +7,12 @@ public class SellCoin : MonoBehaviour {
 
     public GameObject textBox;
     public GameObject statusBox;
+    public AudioSource cashOne;
+    public AudioSource cashTwo;
+    public int genTone;
 
     public void ClickTheButton () {
-
+        genTone = Random.Range(1, 3);
         if (GlobalCoins.CoinCount == 0) 
         {
         statusBox.GetComponent<Text>().text = "Not enough Coins to sell!";
@@ -17,6 +20,14 @@ public class SellCoin : MonoBehaviour {
         }
         else 
         {
+        if (genTone == 1)
+        {
+            cashOne.Play();
+        }
+        if (genTone == 2)
+        {
+            cashTwo.Play();
+        }
         GlobalCoins.CoinCount -= 1;
         GlobalCash.CashCount += 1;
         }
